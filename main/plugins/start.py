b183ae9750faaa849f97a7e9fc0d10fef7183687
Vinisha <110597375+kd2929@users.noreply.github.com>
2023-04-18 06:30:59 +0530

    Add files via upload

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,29 +1,27 @@
-#Github.com/Vasusen-code
+#Github.com/mrinvisible7
 
 import os
-from .. import bot as Drone
+from .. import bot as Invix
 from telethon import events, Button
 
-from ethon.mystarts import start_srb
+#from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
-@Drone.on(events.callbackquery.CallbackQuery(data="set"))
+@Invix.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
-    Drone = event.client                    
+    Invix = event.client
     button = await event.get_message()
-    msg = await button.get_reply_message() 
+    msg = await button.get_reply_message()
     await event.delete()
-    async with Drone.conversation(event.chat_id) as conv: 
+    async with Invix.conversation(event.chat_id) as conv: 
         xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
         x = await conv.get_reply()
         if not x.media:
             xx.edit("No media found.")
         mime = x.file.mime_type
-        if not 'png' in mime:
-            if not 'jpg' in mime:
-                if not 'jpeg' in mime:
-                    return await xx.edit("No image found.")
+        if 'png' not in mime and 'jpg' not in mime and 'jpeg' not in mime:
+            return await xx.edit("No image found.")
         await xx.delete()
         t = await event.client.send_message(event.chat_id, 'Trying.')
         path = await event.client.download_media(x.media)
@@ -32,9 +30,9 @@ async def sett(event):
         os.rename(path, f'./{event.sender_id}.jpg')
         await t.edit("Temporary thumbnail saved!")
         
-@Drone.on(events.callbackquery.CallbackQuery(data="rem"))
+@Invix.on(events.callbackquery.CallbackQuery(data="rem"))
 async def remt(event):  
-    Drone = event.client            
+    Invix = event.client            
     await event.edit('Trying.')
     try:
         os.remove(f'{event.sender_id}.jpg')
@@ -42,8 +40,21 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+@Invix.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** Bot maker"
-    await start_srb(event, text)
+    text = "👋 Hi, I am 'Save Restricted Content ' bot.\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here.\nFor private channel's messages, send the Invite Link first.\n\n👨🏻‍💻Owner: @mister_invisiblebot.\n**support:** @mr_invisible_bots"
+    #await start_srb(event, text)
+    await event.reply(text, 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="set"),
+                               Button.inline("REM THUMB.", data="rem")],        
+                              ])                             
+                              
+    '''
+    await event.reply(text, 
+                      buttons=[
+                              [Button.inline("SET THUMB.", data="set"),
+                               Button.inline("REM THUMB.", data="rem")],
+                              [Button.url("Maintained and Modified by", url="t.me/mr_invisible_bots")]])
+    '''
     
